1e607ab17a0ab2f88d81e6660eb21e49dd709edf
Tomáš Černý <tmscer@gmail.com>
2019-11-17 11:49:49 +0100

    formatting

--- src/client.tsx
+++ src/client.tsx
@@ -3,19 +3,20 @@ import * as e6p from "es6-promise";
 import "isomorphic-fetch";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {Provider} from "react-redux";
-import {RouterProvider} from "react-router5";
-import {setStylesTarget} from "typestyle";
-import {config as appConfig} from "../config";
-import {App} from "./app/containers/App";
-import {configureStore} from "./app/redux/configureStore";
-import {setLanguage} from "./app/redux/modules/settingsActionCreators";
-import {configureRouter} from "./app/routes/configureRouter";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router5";
+import { setStylesTarget } from "typestyle";
+import { config as appConfig } from "../config";
+import { App } from "./app/containers/App";
+import { configureStore } from "./app/redux/configureStore";
+import { setLanguage } from "./app/redux/modules/settingsActionCreators";
+import { configureRouter } from "./app/routes/configureRouter";
 import rootSaga from "./app/sagas/rootSaga";
 
-const ReactHotLoader = appConfig.env !== "production"
-  ? require("react-hot-loader").AppContainer
-  : ({ children }) => React.Children.only(children);
+const ReactHotLoader =
+  appConfig.env !== "production"
+    ? require("react-hot-loader").AppContainer
+    : ({ children }) => React.Children.only(children);
 
 const renderOrHydrate = appConfig.ssr ? ReactDOM.hydrate : ReactDOM.render;
 
@@ -27,15 +28,13 @@ if (!appConfig.ssr) {
 }
 router.start();
 renderOrHydrate(
-  (
-    <ReactHotLoader>
-      <Provider store={store} key="provider">
-        <RouterProvider router={router}>
-          <App/>
-        </RouterProvider>
-      </Provider>
-    </ReactHotLoader>
-  ),
+  <ReactHotLoader>
+    <Provider store={store} key="provider">
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+    </Provider>
+  </ReactHotLoader>,
   document.getElementById("app")
 );
 
@@ -43,17 +42,15 @@ setStylesTarget(document.getElementById("styles-target"));
 
 if ((module as any).hot) {
   (module as any).hot.accept("./app/containers/App", () => {
-    const {App: NewApp} = require("./app/containers/App");
+    const { App: NewApp } = require("./app/containers/App");
     renderOrHydrate(
-      (
-        <ReactHotLoader>
-          <Provider store={store}>
-            <RouterProvider router={router}>
-              <NewApp/>
-            </RouterProvider>
-          </Provider>
-        </ReactHotLoader>
-      ),
+      <ReactHotLoader>
+        <Provider store={store}>
+          <RouterProvider router={router}>
+            <NewApp />
+          </RouterProvider>
+        </Provider>
+      </ReactHotLoader>,
       document.getElementById("app")
     );
   });
--- src/server.tsx
+++ src/server.tsx
@@ -5,17 +5,17 @@ import express from "express";
 import "isomorphic-fetch";
 import path from "path";
 import * as React from "react";
-import {renderToString} from "react-dom/server";
-import {Provider} from "react-redux";
-import {RouterProvider} from "react-router5";
+import { renderToString } from "react-dom/server";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router5";
 import favicon from "serve-favicon";
-import {config as appConfig} from "../config";
-import {App} from "./app/containers/App";
-import {Html} from "./app/containers/Html";
-import {LanguageHelper} from "./app/helpers/LanguageHelper";
-import {configureStore} from "./app/redux/configureStore";
-import {IStore} from "./app/redux/IStore";
-import {configureRouter} from "./app/routes/configureRouter";
+import { config as appConfig } from "../config";
+import { App } from "./app/containers/App";
+import { Html } from "./app/containers/Html";
+import { LanguageHelper } from "./app/helpers/LanguageHelper";
+import { configureStore } from "./app/redux/configureStore";
+import { IStore } from "./app/redux/IStore";
+import { configureRouter } from "./app/routes/configureRouter";
 import rootSaga from "./app/sagas/rootSaga";
 
 const app = express();
@@ -25,12 +25,14 @@ if (process.env.NODE_ENV !== "production") {
   const webpackConfig = require("../config/webpack/dev");
   const webpackCompiler = webpack(webpackConfig);
 
-  app.use(require("webpack-dev-middleware")(webpackCompiler, {
-    lazy: false,
-    logLevel: "info",
-    publicPath: webpackConfig.output.publicPath,
-    stats: {colors: true}
-  }));
+  app.use(
+    require("webpack-dev-middleware")(webpackCompiler, {
+      lazy: false,
+      logLevel: "info",
+      publicPath: webpackConfig.output.publicPath,
+      stats: { colors: true }
+    })
+  );
 
   app.use(require("webpack-hot-middleware")(webpackCompiler));
 }
@@ -39,14 +41,17 @@ app.use(favicon(path.join(__dirname, "public/favicon.ico")));
 
 app.use("/public", express.static(path.join(__dirname, "public")));
 
-app.get("/translations/:language", (req: express.Request, res: express.Response) => {
-  const languageHelper = new LanguageHelper(req.params.language);
-  res.json(languageHelper.getTranslations());
-});
+app.get(
+  "/translations/:language",
+  (req: express.Request, res: express.Response) => {
+    const languageHelper = new LanguageHelper(req.params.language);
+    res.json(languageHelper.getTranslations());
+  }
+);
 
 app.get("*", (req: express.Request, res: express.Response) => {
   if (!appConfig.ssr) {
-    res.sendFile(path.resolve("./build/index.html"), {}, (error) => {
+    res.sendFile(path.resolve("./build/index.html"), {}, error => {
       if (error) {
         console.error(error.message);
       }
@@ -61,7 +66,9 @@ app.get("*", (req: express.Request, res: express.Response) => {
       return;
     }
 
-    const languageHelper = new LanguageHelper(req.headers["accept-language"] as string);
+    const languageHelper = new LanguageHelper(
+      req.headers["accept-language"] as string
+    );
     const store = configureStore(router, {
       router: {
         previousRoute: null,
@@ -78,31 +85,34 @@ app.get("*", (req: express.Request, res: express.Response) => {
       }
     });
 
-    store.runSaga(rootSaga).toPromise().then(() => {
-      // deep clone state because store will be changed during the second render in constructor
-      const initialState = JSON.parse(JSON.stringify(store.getState()));
+    store
+      .runSaga(rootSaga)
+      .toPromise()
+      .then(() => {
+        // deep clone state because store will be changed during the second render in constructor
+        const initialState = JSON.parse(JSON.stringify(store.getState()));
 
-      // tslint:disable-next-line
-      console.time("second render");
+        // tslint:disable-next-line
+        console.time("second render");
 
-      // render again from the initial data
-      const markup = renderToString(
-        <Provider store={store} key="provider">
-          <RouterProvider router={router}>
-            <App/>
-          </RouterProvider>
-        </Provider>
-      );
+        // render again from the initial data
+        const markup = renderToString(
+          <Provider store={store} key="provider">
+            <RouterProvider router={router}>
+              <App />
+            </RouterProvider>
+          </Provider>
+        );
 
-      // tslint:disable-next-line
-      console.timeEnd("second render");
+        // tslint:disable-next-line
+        console.timeEnd("second render");
 
-      res.status(200).send(renderHTML(markup, initialState));
-
-    }).catch((err: any) => {
-      console.error(err.message);
-      res.status(500).send(err.message);
-    });
+        res.status(200).send(renderHTML(markup, initialState));
+      })
+      .catch((err: any) => {
+        console.error(err.message);
+        res.status(500).send(err.message);
+      });
 
     // tslint:disable-next-line
     console.time("first render");
@@ -111,7 +121,7 @@ app.get("*", (req: express.Request, res: express.Response) => {
     renderToString(
       <Provider store={store} key="provider">
         <RouterProvider router={router}>
-          <App/>
+          <App />
         </RouterProvider>
       </Provider>
     );
@@ -124,20 +134,22 @@ app.get("*", (req: express.Request, res: express.Response) => {
   });
 });
 
-app.listen(appConfig.port, appConfig.host, (err) => {
+app.listen(appConfig.port, appConfig.host, err => {
   if (err) {
     console.error(Chalk.bgRed(err));
   } else {
-    console.info(Chalk.black.bgGreen(
-      `\n\n💂  Listening at http://${appConfig.host}:${appConfig.port}\n`
-    ));
+    console.info(
+      Chalk.black.bgGreen(
+        `\n\n💂  Listening at http://${appConfig.host}:${appConfig.port}\n`
+      )
+    );
   }
 });
 
 function renderHTML(markup: string, initialState: Partial<IStore>): string {
   const manifest = require("../build/manifest.json");
   const html = renderToString(
-    <Html markup={markup} manifest={manifest} initialState={initialState}/>
+    <Html markup={markup} manifest={manifest} initialState={initialState} />
   );
   return `<!doctype html> ${html}`;
 }
